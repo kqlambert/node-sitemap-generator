ca5355fa9214af2fdb5fc4940b62e71b69c0d0c7
Keegan Lambert <kqlambert@gmail.com>
2014-03-07 16:48:02 -0700

    some comments

--- sitemap.js
+++ sitemap.js
@@ -4,7 +4,7 @@
 
 var fs = require("fs"),
 	i = 0,
-	pages = [],
+	pages = [], // add pages that have querystrings attached to them to the pages array ( example: 'blog/query-string', 'blog/dynamic-content' )
 	xml = "<?xml version='1.0' encoding='UTF-8'?>" + "\r\n" + "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>" + "\r\n",
 	site = "change-to-your-sitename.com",
 	ext = true,
@@ -14,19 +14,23 @@ var fs = require("fs"),
 		output: process.stdout
 	});
 
+// asks the name of the site will default to change-to-your-sitename.com if no data given
 rl.question("What is your site name? (example: your-site-name.com)", function(answer) {
 	"use strict";
 	if (answer) {
 		site = answer;
 	}
+	// asks if you would like to include file extensions defaults to yes
 	rl.question("Would you like to include file extensions? (example: .html) [y/n]", function(answer2) {
 		if (answer2) {
 			if (answer2.toLowerCase() !== "y") {
 				ext = false;
 			}
 		}
+		// specifies directory
 		fs.readdir("./", function (err, list) {
 			if (err) throw err;
+			// adds found files to pages array
 			for (i = 0; i < list.length; i++) {
 				if (list[i].indexOf(".html") !== -1 || list[i].indexOf(".php") !== -1) {
 					if (list[i] === "index.html" || list[i] === "index.php") {
@@ -36,6 +40,7 @@ rl.question("What is your site name? (example: your-site-name.com)", function(an
 					}
 				}
 			}
+			// begins formating each file into xml for sitemap
 			for (i = 0; i < pages.length; i++) {
 				var mtdate = new Date(fs.statSync("./" + pages[i]).mtime),
 					currentDate = mtdate.getFullYear() + "-";
@@ -62,18 +67,22 @@ rl.question("What is your site name? (example: your-site-name.com)", function(an
 				xml += "\t\t" + "<priority>" + priority.toFixed(1) + "</priority>" + "\r\n\t" + "</url>" + "\r\n";
 			}
 			xml += "</urlset>";
+			// creates sitemap.xml
 			fs.writeFile("sitemap.xml", xml, function (err) {
 				if (err) throw err;
 				console.log("sitemap.xml has been saved!");
 			});
+			// begins formating for robots.txt
 			var robots = "User-agent: *" + "\r\n";
 			robots += "Allow: *" + "\r\n";
 			robots += "Sitemap: http://" + site + "/sitemap.xml";
+			// creates robots.txt
 			fs.writeFile("robots.txt", robots, function (err) {
 				if (err) throw err;
 				console.log("robots.txt has been saved!");
 			});
 		});
+		// closes question prompt
 		rl.close();
 	});
 });
